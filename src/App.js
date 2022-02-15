c6cc3feb5f51a2a3a1de62b0d8f5fe37a32b022f
Mariana García Cervantes <mariangace@gmail.com>
2022-02-15 15:24:45 +0100

    hide example modal

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "./styles.css";
 
 function App() {
   const modalRef = useRef();
-  const modalExaple2 = useRef();
+  //const modalExaple2 = useRef();
 
   return (
     <div className="App">
@@ -19,94 +19,94 @@ function App() {
       <LoadingModal ref={modalRef}>
         <img alt="loading" src={Rocket} />
       </LoadingModal>
-      <button onClick={() => modalExaple2.current.open()}>Open Modal</button>
-      <Modal ref={modalRef}>
+      {/*<button onClick={() => modalExaple2.current.open()}>Open Modal</button>
+       <Modal ref={modalRef}>
         <h1>Hello world</h1>
-      </Modal>
+      </Modal> */}
     </div>
   );
 }
 
 export default App;
 
-const Modal = forwardRef((props, ref) => {
-  const [open, setOpen] = useState(true);
+// const Modal = forwardRef((props, ref) => {
+//   const [open, setOpen] = useState(true);
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => setOpen(true),
-      close: () => setOpen(false),
-    };
-  });
+//   useImperativeHandle(ref, () => {
+//     return {
+//       open: () => setOpen(true),
+//       close: () => setOpen(false),
+//     };
+//   });
 
-  return (
-    <AnimatePresence>
-      {open && (
-        <>
-          <motion.div
-            className="modal-backdrop"
-            initial={{
-              opacity: 0,
-            }}
-            animate={{
-              opacity: 1,
-              transition: {
-                duration: 0.3,
-              },
-            }}
-            exit={{
-              opacity: 0,
-              transition: {
-                delay: 0.3,
-              },
-            }}
-            onClick={() => setOpen(false)}
-          >
-            <motion.div
-              initial={{ scale: 0 }}
-              animate={{
-                scale: 1,
-                transition: {
-                  duration: 0.3,
-                },
-              }}
-              exit={{
-                scale: 0,
-                transition: {
-                  delay: 0.3,
-                },
-              }}
-              className="modal-content-wrapper"
-            >
-              <motion.div
-                initial={{ x: 100 }}
-                animate={{
-                  x: 0,
-                  opacity: 1,
-                  transition: {
-                    delay: 0.3,
-                    duration: 0.3,
-                  },
-                }}
-                exit={{
-                  x: 100,
-                  opacity: 0,
-                  transition: { duration: 0.3 },
-                }}
-                className="modal-content"
-              >
-                {props.children}
-              </motion.div>
-            </motion.div>
-          </motion.div>
-        </>
-      )}
-    </AnimatePresence>
-  );
-});
+//   return (
+//     <AnimatePresence>
+//       {open && (
+//         <>
+//           <motion.div
+//             className="modal-backdrop"
+//             initial={{
+//               opacity: 0,
+//             }}
+//             animate={{
+//               opacity: 1,
+//               transition: {
+//                 duration: 0.3,
+//               },
+//             }}
+//             exit={{
+//               opacity: 0,
+//               transition: {
+//                 delay: 0.3,
+//               },
+//             }}
+//             onClick={() => setOpen(false)}
+//           >
+//             <motion.div
+//               initial={{ scale: 0 }}
+//               animate={{
+//                 scale: 1,
+//                 transition: {
+//                   duration: 0.3,
+//                 },
+//               }}
+//               exit={{
+//                 scale: 0,
+//                 transition: {
+//                   delay: 0.3,
+//                 },
+//               }}
+//               className="modal-content-wrapper"
+//             >
+//               <motion.div
+//                 initial={{ x: 100 }}
+//                 animate={{
+//                   x: 0,
+//                   opacity: 1,
+//                   transition: {
+//                     delay: 0.3,
+//                     duration: 0.3,
+//                   },
+//                 }}
+//                 exit={{
+//                   x: 100,
+//                   opacity: 0,
+//                   transition: { duration: 0.3 },
+//                 }}
+//                 className="modal-content"
+//               >
+//                 {props.children}
+//               </motion.div>
+//             </motion.div>
+//           </motion.div>
+//         </>
+//       )}
+//     </AnimatePresence>
+//   );
+// });
 
 const LoadingModal = forwardRef((props, ref) => {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const DURATION = 1.5;
   useImperativeHandle(ref, () => {
     return {
